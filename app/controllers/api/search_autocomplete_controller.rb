db9f9e8003d7b6f2c05ce1c7063602fdac638d0d
Omar Torres <torresomar44@gmail.com>
2017-02-10 12:59:35 -0500

    Add and show tag search in elasticsearch again

--- app/controllers/api/search_autocomplete_controller.rb
+++ app/controllers/api/search_autocomplete_controller.rb
@@ -1,7 +1,8 @@
 class API::SearchAutocompleteController < ApplicationController
   def index
-    results = Elasticsearch::Model.search(params[:term], [Post, User])
+    results = Elasticsearch::Model.search(params[:term], [Post, User, Tag])
     @posts = results.select { |result| result["_type"] == 'post' }
     @users = results.select { |result| result["_type"] == 'user' }
+    @tags  = results.select { |result| result["_type"] == 'tag' }
   end
 end
